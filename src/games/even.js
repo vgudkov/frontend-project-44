6b512ad70410189d0bbffd670f5b90444dd6477c
Vladimir Gudkov <vgudkov777@gmail.com>
2023-06-16 00:42:39 +0700

    games directory moved to src/games

---
+++ src/games/even.js
@@ -0,0 +1,28 @@
+import readlineSync from 'readline-sync';
+import {
+  getRandomNumber, isEven, getWrongAnswer, showWinner,
+} from '../src/add_functions.js';
+
+console.log('Welcome to the Brain Games!');
+const playerName = readlineSync.question('May I have your name? ');
+
+console.log(`Hello, ${playerName}!`);
+console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+export default () => {
+  for (let i = 0; i <= 2; i += 1) {
+    const randomNumber = getRandomNumber(101);
+    console.log(`Question: ${randomNumber}`);
+    const evenAnswer = readlineSync.question('Your answer: ');
+
+    if (evenAnswer === 'yes' && isEven(randomNumber)) {
+      console.log('Correct!');
+    } else if (evenAnswer === 'no' && !isEven(randomNumber)) {
+      console.log('Correct!');
+    } else {
+      return getWrongAnswer(playerName, 'yes', 'no');
+    }
+  }
+
+  return showWinner(playerName);
+};
---
+++ src/games/prime.js
@@ -0,0 +1,28 @@
+import readlineSync from 'readline-sync';
+import {
+  getRandomNumber, isPrime, getWrongAnswer, showWinner,
+} from '../src/add_functions.js';
+
+console.log('Welcome to the Brain Games!');
+const playerName = readlineSync.question('May I have your name? ');
+
+console.log(`Hello, ${playerName}!`);
+console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
+
+export default () => {
+  for (let i = 0; i <= 2; i += 1) {
+    const randomNumber = getRandomNumber(101);
+    console.log(`Question: ${randomNumber}`);
+    const primeAnswer = readlineSync.question('Your answer: ');
+
+    if (primeAnswer === 'yes' && isPrime(randomNumber)) {
+      console.log('Correct!');
+    } else if (primeAnswer === 'no' && !isPrime(randomNumber)) {
+      console.log('Correct!');
+    } else {
+      return getWrongAnswer(playerName, 'yes', 'no');
+    }
+  }
+
+  return showWinner(playerName);
+};
